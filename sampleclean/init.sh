6d9321256e8f266696dfc5baed6f67d97144d838
Daniel Haas <dhaas@eecs.berkeley.edu>
2014-10-17 14:28:59 -0700

    Added setup scripts for sampleclean.

---
+++ sampleclean/init.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+pushd /root
+
+if [ -d "sampleclean-async" ]; then
+  echo "Sampleclean seems to be installed. Exiting."
+  popd
+  return 0
+fi
+
+SAMPLECLEAN_VERSION = 0.0.1
+
+# Github tag:
+if [[ "$SAMPLECLEAN_VERSION" == *\|* ]]
+then
+  # Not yet supported
+  echo ""
+# clone the sampleclean git repo
+else
+    git clone https://github.com/sjyk/sampleclean-async.git
+fi
+
+# Pre-package tachyon version
+#else
+#  case "$TACHYON_VERSION" in
+#    0.3.0)
+#      wget https://s3.amazonaws.com/Tachyon/tachyon-0.3.0-bin.tar.gz
+#      ;;
+#    0.4.0)
+#      wget https://s3.amazonaws.com/Tachyon/tachyon-0.4.0-bin.tar.gz
+#      ;;
+#    0.4.1)
+#      wget https://s3.amazonaws.com/Tachyon/tachyon-0.4.1-bin.tar.gz
+#      ;;
+#    *)
+#      echo "ERROR: Unknown Tachyon version"
+#      return -1
+#  esac
+
+#  echo "Unpacking Tachyon"
+#  tar xvzf tachyon-*.tar.gz > /tmp/spark-ec2_tachyon.log
+#  rm tachyon-*.tar.gz
+#  mv `ls -d tachyon-*` tachyon
+#fi
+
+popd
