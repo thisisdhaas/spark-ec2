1cf4a8d2ff40e92cebd393626c246f8a9ed4a117
Daniel Haas <dhaas@eecs.berkeley.edu>
2014-10-19 14:18:16 -0700

    Fixing directory navigation in sampleclean setup.

--- sampleclean/init.sh
+++ sampleclean/init.sh
@@ -32,6 +32,7 @@ else
     # Fix the authentication configuration
     pushd /root/spark-ec2/sampleclean
     cp pg_hba.conf /var/lib/pgsql92/data/pg_hba.conf
+    popd
 
     # Start postgres
     chkconfig postgresql92 on
@@ -48,6 +49,7 @@ else
     wget http://www.rabbitmq.com/releases/rabbitmq-server/v3.3.5/rabbitmq-server-3.3.5-1.noarch.rpm
     rpm --import http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
     yum install -y rabbitmq-server-3.3.5-1.noarch.rpm
+    rm rabbitmq-server-3.3.5-1.noarch.rpm
     echo " Done!"
 
     # Install python2.7 and create a virtualenv
@@ -56,28 +58,32 @@ else
     wget wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | python27
     easy_install-2.7 pip
     pip2.7 install virtualenvwrapper
+    rm setuptools-7.0.zip
+    pushd /root/spark-ec2/sampleclean
     cat virtualenv_setup.sh >> /root/.bash_profile
+    popd
     source /root/.bash_profile
     mkvirtualenv sampleclean
     workon sampleclean
-    popd
     echo " Done!"
 
     # Install python requirements
     echo "\n\t Installing matplotlib..."
-    pushd sampleclean-async/src/main/python/crowd_server
     yum install -y libpng-devel
     # Crazy hacks to get matplotlib working
     pip install --download . matplotlib==1.4.0 # this will fail to build, but download successfully
     tar xzf matplotlib-1.4.0.tar.gz
-    cp setupext.py matplotlib-1.4.0/setupext.py
+    cp spark-ec2/sampleclean/setupext.py matplotlib-1.4.0/setupext.py
     rm matplotlib-1.4.0.tar.gz
     tar czf matplotlib-1.4.0.tar.gz matplotlib-1.4.0
     pip install matplotlib-1.4.0.tar.gz
+    rm matplotlib-1.4.0.tar.gz
+    rm -rf matplotlib-1.4.0
     echo " Done!"
 
     # and the rest of the python packages
     echo "\n\t Installing remaining python requirements..."
+    pushd sampleclean-async/src/main/python/crowd_server
     pip2.7 install -r requirements.txt
     popd
     echo " Done!"
