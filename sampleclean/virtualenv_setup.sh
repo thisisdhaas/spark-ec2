2bd47c77203fb6e0a30a0a39d0ea98cdb7d72620
Daniel Haas <dhaas@eecs.berkeley.edu>
2015-04-28 15:20:26 -0700

    Fix path for virtualenv script during install.

--- sampleclean/virtualenv_setup.sh
+++ sampleclean/virtualenv_setup.sh
@@ -1,7 +1,7 @@
 export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2.7
 export WORKON_HOME=/root/.env
 export PROJECT_HOME=/root/sampleclean-async/src/main/python/crowd_server
-source /usr/bin/virtualenvwrapper.sh
+source /usr/local/bin/virtualenvwrapper.sh
 
 export C_FORCE_ROOT=1
 export PGDATA=/mnt/sampleclean/data/
