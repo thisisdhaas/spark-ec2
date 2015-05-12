d1b2056d9fe5a1febda27561704192f395d665da
Daniel Haas <dhaas@eecs.berkeley.edu>
2015-05-12 12:49:04 -0700

    Set AMT callback differently if ssl certificates are installed.

--- sampleclean/virtualenv_setup.sh
+++ sampleclean/virtualenv_setup.sh
@@ -2,7 +2,11 @@ export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2.7
 export WORKON_HOME=/root/.env
 export PROJECT_HOME=/root/ampcrowd/
 source /usr/local/bin/virtualenvwrapper.sh
-export AMT_CALLBACK_HOST=`cat /root/spark-ec2/master-domain`
+if [ -f "/root/spark-ec2/master-domain" ]; then
+    export AMT_CALLBACK_HOST=`cat /root/spark-ec2/master-domain`
+else
+    export AMT_CALLBACK_HOST=`cat /root/spark-ec2/cluster-url | cut -d ':' -f 2 | cut -d "/" -f 3`
+fi
 
 export C_FORCE_ROOT=1
 export PGDATA=/mnt/ampcrowd/data/
