a8b59f97b1d1c80575cb65a5530137019b1208f4
Daniel Haas <dhaas@eecs.berkeley.edu>
2015-05-12 13:24:54 -0700

    Better environment variable setting.

--- sampleclean/virtualenv_setup.sh
+++ sampleclean/virtualenv_setup.sh
@@ -1,12 +1,9 @@
+
 export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2.7
 export WORKON_HOME=/root/.env
 export PROJECT_HOME=/root/ampcrowd/
 source /usr/local/bin/virtualenvwrapper.sh
-if [ -f "/root/spark-ec2/master-domain" ]; then
-    export AMT_CALLBACK_HOST=`cat /root/spark-ec2/master-domain`
-else
-    export AMT_CALLBACK_HOST=`cat /root/spark-ec2/cluster-url | cut -d ':' -f 2 | cut -d "/" -f 3`
-fi
-
+source /root/spark-ec2/ec2-variables.sh
+export AMT_CALLBACK_HOST=$MASTER_DOMAIN
 export C_FORCE_ROOT=1
 export PGDATA=/mnt/ampcrowd/data/
